b0d1bee010383383a445cc4a6d88b0360a2588ea
Peiyun Lee <may1092200258@gmail.com>
2022-06-07 15:31:17 +0800

    Initial Commit

---
+++ Greenabal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Greenabal
+//
+//  Created by Pei-yun,Lee on 2022/6/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Greenabal/GreenabalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenabalApp.swift
+//  Greenabal
+//
+//  Created by Pei-yun,Lee on 2022/6/7.
+//
+
+import SwiftUI
+
+@main
+struct GreenabalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
