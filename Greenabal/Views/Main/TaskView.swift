8b442e514381308dd7f4d931b56d500a4760fad2
yuudachi31 <yuudachi31@gmail.com>
2022-06-09 17:39:17 +0800

    feat:change to task

--- Greenabal/Views/Main/TaskView.swift
+++ Greenabal/Views/Main/TaskView.swift
@@ -6,10 +6,79 @@
 //
 
 import SwiftUI
-
+struct ListData
+{
+    var taskClass:String
+    var taskDetail:String
+    var taskReward:Int
+}
 struct TaskView: View {
     var body: some View {
-        Text("TaskView")
+        
+        VStack {
+            Text("今日任務")
+            .frame(
+                 
+                  maxWidth: .infinity
+            ).frame( height: 48).background(.white)
+            VStack(spacing:13){
+                HStack{
+                    let leafNum:Int=2
+                    Button{
+                        
+                    }label:{
+                            Image("play-circle").frame(width: 48,
+                                                       height: 48)
+                    }
+                    Text("閱讀環保知識").font(.system(size: 14))
+                    Spacer()
+                        
+                    Text("\(leafNum)").font(.system(size: 14))
+                        .overlay {
+                               LinearGradient(
+                                colors: [Color(red: 18/255, green: 235/255,blue: 195/255),Color(red: 7/255, green: 215/255,blue: 66/255) ],
+                                   startPoint: .leading,
+                                   endPoint: .trailing
+                               ) .mask(
+                                Text("\(leafNum)")
+                                    .font(Font.system(size: 14))
+                                    .multilineTextAlignment(.center)
+                            )
+                           }
+                    Image("leaf")
+                    Spacer().frame(width: 15, height: 40)
+                }.background(.white).cornerRadius(10)
+                HStack{
+                    let leafNum:Int=2
+                    Button{
+                        
+                    }label:{
+                            Image("play-circle").frame(width: 48,
+                                                       height: 48)
+                    }
+                    Text("閱讀環保知識").font(.system(size: 14))
+                    Spacer()
+                        
+                    Text("\(leafNum)").font(.system(size: 14))
+                        .overlay {
+                               LinearGradient(
+                                colors: [Color(red: 18/255, green: 235/255,blue: 195/255),Color(red: 7/255, green: 215/255,blue: 66/255) ],
+                                   startPoint: .leading,
+                                   endPoint: .trailing
+                               ) .mask(
+                                Text("\(leafNum)")
+                                    .font(Font.system(size: 14))
+                                    .multilineTextAlignment(.center)
+                            )
+                           }
+                    Image("leaf")
+                    Spacer().frame(width: 15, height: 40)
+                    
+                }.background(.white).cornerRadius(10)
+                Spacer()
+            }.padding(.horizontal,27)
+        }.background(.blue)
+        
     }
 }
 
