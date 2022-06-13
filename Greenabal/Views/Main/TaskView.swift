c30a1af6864726f272d2a46ecb15552f43d3b9d2
yuudachi31 <yuudachi31@gmail.com>
2022-06-13 22:54:37 +0800

    taskview ok

--- Greenabal/Views/Main/TaskView.swift
+++ Greenabal/Views/Main/TaskView.swift
@@ -18,7 +18,107 @@ struct taskListData:Identifiable
     var taskDetail:String
     var taskReward:Int
 }
+struct DraggableModifier : ViewModifier {
+    enum Direction {
+        case vertical
+        case horizontal
+    }
 
+    let direction: Direction
+    @State private var reLoadWidth:CGFloat=CGFloat(0)
+    @State private var draggedOffset: CGSize = .zero
+    @State private var BreakPoint:CGSize = .zero
+    @State private var isShowIcon:Bool = false
+    @State private var IconOpacity:Double = 0
+    @State private var frameOpacity:Double = 0
+//    @State private var frameCornerR:Double = 0
+
+    func body(content: Content) -> some View {
+        HStack{
+        content
+        .offset(
+            CGSize(width: direction == .vertical ? 0 : draggedOffset.width,
+                   height: direction == .horizontal ? 0 : draggedOffset.height)
+        )
+        .gesture(
+            DragGesture()
+            .onChanged { value in
+//               if self.draggedOffset.width > CGFloat(-40) && self.draggedOffset.width <= CGFloat(0){
+                if self.BreakPoint.width == CGFloat(0){
+                    if value.translation.width >= CGFloat(-52) && value.translation.width <= CGFloat(0){
+                    self.draggedOffset = CGSize(width:self.BreakPoint.width + value.translation.width,height:self.BreakPoint.height + value.translation.height)
+                        self.reLoadWidth = CGFloat(value.translation.width * -1 - 2)
+                        if value.translation.width <= CGFloat(-12){
+                            self.frameOpacity =  ( -12 - value.translation.width )/40
+                        }
+                        if value.translation.width <= CGFloat(-32){
+                            self.IconOpacity =  ( -32 - value.translation.width )/20
+//                            self.frameCornerR = (-32 - value.translation.width)/2
+                        }
+                    }
+                    
+                }else if self.BreakPoint.width == CGFloat(-50){
+                    if value.translation.width <= CGFloat(52) && value.translation.width >= CGFloat(0){
+                    self.draggedOffset = CGSize(width:self.BreakPoint.width + value.translation.width,height:self.BreakPoint.height + value.translation.height)
+                        self.reLoadWidth = CGFloat(46 - value.translation.width )
+                        if value.translation.width <= CGFloat(40){
+                            self.frameOpacity =  (40 - value.translation.width )/40
+                            
+                        }else{
+                            self.frameOpacity = 0
+                        }
+                        if value.translation.width <= CGFloat(20){
+                            self.IconOpacity =  (20 - value.translation.width )/20
+                        }else{
+                           
+                            self.IconOpacity = 0
+                        }
+                    }
+                }
+                
+                
+                
+//                print(value.translation)
+//                print(value.startLocation)
+//                    print(self.draggedOffset.width)
+                    
+//                }
+            }
+            .onEnded { value in
+                if value.translation.width < -30{
+                    self.draggedOffset = CGSize(width:-50 , height:0 )
+                    self.BreakPoint = CGSize(width:-50 , height:0 )
+                    self.reLoadWidth=CGFloat(48)
+                    self.IconOpacity=1
+                    self.frameOpacity = 1
+//                    isShowIcon.toggle()
+                }else{
+                    self.draggedOffset = .zero
+                    self.BreakPoint = .zero
+                    self.reLoadWidth=CGFloat(0)
+                    self.IconOpacity=0
+                    self.frameOpacity = 0
+//                    isShowIcon.toggle()
+                }
+                
+            }
+        ).overlay(HStack{Spacer()
+            ZStack{Text("").frame(width: self.reLoadWidth, height: 48 ).background(.white).cornerRadius(10).opacity(self.frameOpacity)
+                Button{
+                    print("reload!")
+                } label: {
+                    Image("reLoad").resizable().frame(width: 16, height: 16).opacity(self.IconOpacity)
+                }
+                
+            
+                
+            }
+        },alignment: .top)
+            
+        }
+    }
+
+}
 struct TaskView: View {
     
     @EnvironmentObject var background: BackgroundViewModel
@@ -29,12 +129,16 @@ struct TaskView: View {
         taskListData(finish:false, taskClass: "2",taskDetail: "自備購物袋/塑膠袋",taskReward: 2),
         taskListData(finish:false, taskClass: "2",taskDetail: "檢查並拔掉無使用的插頭",taskReward: 2)
     ]
-    
+   
 //    @State private var taskCount:Int = taskListDataArray.count
 //    @State private var finishedTaskCount:Int = taskListDataArray.filter{$0.finish==true}.count
     private let title = "每日任務"
     private let name = "Task"
-    
+    init() {
+       UITableView.appearance().separatorStyle = .none
+       UITableViewCell.appearance().backgroundColor = .clear
+       UITableView.appearance().backgroundColor = .clear
+    }
     var body: some View {
         VStack(){
             Header(title: title, name: name)
@@ -49,17 +153,18 @@ struct TaskView: View {
                         Text("完成\(taskListDataArray.filter{$0.finish==true}.count)/\(taskListDataArray.count)")
                     }.padding(EdgeInsets(top: 10, leading: 0, bottom: 5, trailing: 0))
                    
-                       
+                   
+                        
                         ForEach(0..<taskListDataArray.count,id: \.self)  { item in
                                        
                             EachTaskView(item:item,taskListDataArray:$taskListDataArray)
                             
-                                   }
-                        
+                                   }.listRowSeparator(.hidden)
+                }.padding(.horizontal,27)
 
                         
                         Spacer()
-                    }.padding(.horizontal,27)
+                  
                 
             }
             .frame(maxWidth: .infinity,
@@ -73,9 +178,17 @@ struct TaskView: View {
 
 
 struct EachTaskView: View {
+    
     var item:Int
     @Binding var taskListDataArray:[taskListData]
-    
+//     @State var reLoadWidth:CGFloat = CGFloat(0)
+    @State private var location: CGPoint = CGPoint(x: 50, y: 50)
+//    var simpleDrag: some Gesture {
+//            DragGesture()
+//                .onChanged { value in
+//                    print("aaa")
+//                }
+//        }
     var body: some View {
         switch taskListDataArray[item].taskClass{
         case "1":
@@ -106,7 +219,7 @@ struct EachTaskView: View {
                 Spacer().frame(width: 15, height: 40)
             }.background(.white).cornerRadius(10)
         case "2":
-            
+           
             HStack{
                 
                 Button{
@@ -151,6 +264,13 @@ struct EachTaskView: View {
                 Image("leaf")
                 Spacer().frame(width: 15, height: 40)
             }.background(.white).cornerRadius(10)
+                    .modifier(DraggableModifier(direction: .horizontal))
+//                    .gesture(
+//                                    simpleDrag
+//                                )
+//                Text("").frame(width: 48, height: 48 )
+//                Text("").frame(width: self.reLoadWidth, height: 48 )
+            
         default:
             Text("3: " + taskListDataArray[item].taskDetail)
         }
