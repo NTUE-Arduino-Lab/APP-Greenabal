6c977fb88ff39ec31951aa84af53aa58deb0bed9
yuudachi31 <yuudachi31@gmail.com>
2022-06-17 16:59:24 +0800

    fix: island image

--- Greenabal/Views/Main/AchivementView/IslandCollection.swift
+++ Greenabal/Views/Main/AchivementView/IslandCollection.swift
@@ -14,7 +14,7 @@ struct IslandCollection: View {
         VStack{
             Text("初來乍島").font(.custom("Roboto Bold", size: 18)).tracking(1.44)
             Text("No. 168").font(.custom("Roboto Medium", size: 16)).tracking(0.64).padding(.top,10)
-            Image("islandCollection-1").padding(.vertical,28)
+            Image("IslandCollection-1").padding(.vertical,28)
             Text("Lv. 3").font(.custom("Roboto Medium", size: 18)).tracking(0.72).padding(.bottom,10)
             Text("綠化天數：12 天").font(.custom("Roboto Medium", size: 18)).tracking(0.72)
             Text("").frame(width: .infinity, height: 20).background(Color.clear)
